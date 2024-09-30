fc68a6d8148fbbe4880589989dbc1328c0db4819
TRENDFORCE/maxcheng <ilvnr999@gmail.com>
2024-09-30 16:59:16 +0800

    use list grab the  url and categories of rss first

--- line_rss.py
+++ line_rss.py
@@ -7,8 +7,7 @@ from selenium import webdriver
 from line_url import fetch
 
 
-def grab(cat, url , file_type):
-    url_content = []
+def grab(cat, url):
     # 設定 WebDriver
     driver = webdriver.Chrome()  # 確保你已經安裝了 ChromeDriver
     driver.get(url)
@@ -16,7 +15,6 @@ def grab(cat, url , file_type):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     # 等待頁面加載
     time.sleep(1)  # 可以根據需要調整這個時間
-
     html_content = driver.page_source
     soup = BeautifulSoup(html_content, 'html.parser')
 
@@ -30,34 +28,55 @@ def grab(cat, url , file_type):
         else:
             h2 = cat
 
+        if h2 not in rss:
+            rss[h2] = []
+
         #print(h2)  # 輸出 data-anchor-id 和 h2 的文本內容
         links = div.find_all('a', class_='ltcp-link')  # 在 <h2> 中查找所有 class 為 ltcp-link 的 <a>
         if links:  # 如果找到一個或多個 <a>
-            hrefs = [link['href'] for link in links ]  # 獲取所有 href 值
+            for link in links:
+                half_url = link['href']
+                if 'article' in half_url:
+                    url = 'https://today.line.me' + half_url
+                    rss[h2].append(url)
+
+
+            '''hrefs = [link['href'] for link in links ]  # 獲取所有 href 值
+            print(h2, hrefs)
             for href in hrefs:
                 if 'article' in href :
-                    if '60VLrQw' in href:
-                        continue
                     url = 'https://today.line.me' + href
-                    output = fetch(url, h2)
+                    #output = fetch(url, h2)
                     if output != 0:
                         url_content.append(output)
-                    print(output)
+                    print(output)'''
     driver.quit()
 
+def read_url():
+    for cat, urls in rss.items():
+        for url in urls:
+            output = fetch(url, cat)
+            if output != 0:
+                url_content.append(output)
+
+def save(): 
     # 讀取行名稱
     fieldnames = list(url_content[0].keys())
     print('column names',fieldnames)
 
-    csv_name = 'csv/line.csv'
-    with open(csv_name, file_type, encoding='utf-8', newline='') as file_obj:
+    csv_name = 'csv/line_new.csv'
+    with open(csv_name, 'w', encoding='utf-8', newline='') as file_obj:
         writer = csv.DictWriter(file_obj, fieldnames=fieldnames)
         writer.writeheader()
         for row in url_content:
             writer.writerow(row)
 
 if __name__ == '__main__':
+    rss = {}
+    url_content = []
     url1 = 'https://today.line.me/tw/v3/page/finance'
-    grab('理財,' ,url1, 'w')
+    grab('理財,' ,url1)
     url2 = 'https://today.line.me/tw/v3/page/tech'
-    grab('科技,', url2, 'a')
+    grab('科技,', url2)
+    read_url()
+    save()
