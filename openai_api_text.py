0fffd96185c58844a9110c70967f0d3f7711abe3
TRENDFORCE/maxcheng <ilvnr999@gmail.com>
2024-10-15 14:20:42 +0800

    extract the prop noun and translate ,output the list directly

--- openai_api_text.py
+++ openai_api_text.py
@@ -9,7 +9,7 @@ client = OpenAI()
 
 def read_csv(path):
     df = pd.read_csv(path)
-    content = df["content"]
+    content = df["Content"]
     return content 
 
 def batch(model, max_token, content_list, prompt):
@@ -24,7 +24,7 @@ def batch(model, max_token, content_list, prompt):
             batch_list.append(current_batch)
             current_batch = ""
             current_token = estimated_prompt_token
-        current_batch += content.replace("\n", "") + "\n"
+        current_batch += content.replace("\n", "")
         current_token += content_token
     if current_batch:
         batch_list.append(current_batch.strip())
@@ -37,7 +37,7 @@ def extract_tech_terms(content, model, prompt):
         messages=[
             {"role": "system",
              "content": prompt},
-            {"role": "user", "content":f"Here are some articles separated by ' ':{content}"}
+            {"role": "user", "content":f"Here are some articles:{content}"}
         ],
         temperature=0,
         top_p=1,
@@ -49,31 +49,38 @@ def extract_tech_terms(content, model, prompt):
     total_tokens = usage.total_tokens
     return response.choices[0].message.content.strip(), prompt_tokens, completion_tokens, total_tokens
 
+def split_terms(terms_str):
+    terms = [s.strip().split(':') for s in terms_str.split(",")]
+    return terms
+
 def save_csv(path, terms):
     if not os.path.exists(path) or os.path.getsize(path) == 0:
         mode = 'w'
     else: 
         mode = 'a'
-    terms = dict(Counter(terms))
-    print(terms)
-    df = pd.DataFrame(list(terms.items()), columns=['名詞', '數量'],index=False)
-    df.to_csv(path, mode=mode)
+    #terms = dict(Counter(terms))
+    #print(terms)
+    #df = pd.DataFrame(list(terms.items()), columns=['名詞', '數量'],)
+    df = pd.DataFrame(terms, columns=['英文', '繁中', '簡中'], )
+    df.to_csv(path, mode=mode, index=False)
 
 def main():
     model = "gpt-4o"
     prompt = "You are a model that extracts all proper nouns, technical terms, and other nouns that have different expressions in Simplified and \
-            Traditional Chinese. The input consists of multiple articles separated by the delimiter ' '. Please ensure that you accurately extract \
+            Traditional Chinese. The input consists of multiple articles. Please ensure that you accurately extract \
             terms from each article, recognizing this delimiter as the boundary between different articles. For example, for ‘Nvidia’, you should \
             return ‘英偉達’ and ‘輝達’. Additionally, include terms like ‘製程’ and ‘工藝’, or ‘雲端運算’ and ‘雲計算’.\
             After extracting the terms, translate based on the context:\
             - If the term is in English, translate it into Traditional Chinese and Simplified Chinese.\
             - If the term is in Traditional Chinese, translate it into English and Simplified Chinese.\
             - If the term is in Simplified Chinese, translate it into English and Traditional Chinese.\
-            The output format should be:\
-            English: Traditional Chinese: Simplified Chinese\
-            Different proper nouns should still be separated by commas."
+            The output should format each term by first providing the English term, followed by a colon, \
+            then the Traditional Chinese term with another colon, and finally the Simplified Chinese term. \
+            The output should be formatted as follows: Each term should be presented in the format \
+            'English: Traditional Chinese: Simplified Chinese'. Please ensure there are no numbers or additional \
+            text in the output.Different proper nouns should be separated by commas."
     max_token = 8000
-    read_path = 'tech_news/technews-08_1.csv'
+    read_path = 'csv/line_api.csv'
     save_path = 'tech_news/terms_translate.csv'
     terms_list = []
     contents = read_csv(read_path)
@@ -83,8 +90,9 @@ def main():
     for cont in batch_list:
         terms_str, prom_token, re_token, tot_token = extract_tech_terms(cont, model, prompt)
         print(prom_token, re_token, tot_token)
-        terms = [s.strip() for s in terms_str.split(",")]
-        print(terms)
+        print("terms_str:", terms_str)
+        terms = split_terms(terms_str)
+        print("terms after split:",terms)
         terms_list.extend(terms)
     save_csv(save_path, terms_list)
 
